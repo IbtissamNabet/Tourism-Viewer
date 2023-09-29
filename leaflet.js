f49d0e3e9e6e9cd202b38b82dd8e12a0fe9d9d23
Noellie PAULUS <noelliepaulus@etu.univ-lyon1.fr>
2023-09-29 16:10:24 +0200

    "reset de l'espace de travail"
    séparation unique entre html js et css, fichier(s) javascript non encore ordonnés

--- leaflet.js
+++ leaflet.js
@@ -1,25 +1,53 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+// initialisation pour l'ouverture de la carte, ici Londres
+let map = L.map('map').setView([51.505, -0.09], 13);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+// copyright
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
-var circle = L.circle([51.508, -0.11], {
+// 
+let marker = L.marker([51.5, -0.09]).addTo(map);
+
+// faire apparaître un cercle en fonction de ses coordonées
+let circle = L.circle([51.508, -0.11], {
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5,
     radius: 500
 }).addTo(map);
 
-var polygon = L.polygon([
+let polygon = L.polygon([
     [51.509, -0.08],
     [51.503, -0.06],
     [51.51, -0.047]
 ]).addTo(map);
 
-var popup = L.popup();
-///affiche dans un pop-up les coordonnées s'il n'y a pas de message
+let popup = L.popup();
+
+// affiche dans un pop-up les coordonnées s'il n'y a pas de message
 function onMapClick(e) {
     popup
         .setLatLng(e.latlng)
         .setContent("You clicked the map at " + e.latlng.toString())
         .openOn(map);
 }
+
+// bindPopup: lie un pop-up à un objet déclaré dans une variable
+// openPopup: affiche le pop-up en solo (ferme les autres)
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+// openOn: comme addOn(dans leaflet.js), sauf qu'il gère automatiquement 
+// les ouvertures et fermetures de pop-up quand tu en cliques sur un autre
+
+// popup d'exemple en mode texte
+popup
+.setLatLng([51.513, -0.09])
+.setContent("I am a standalone popup.")
+.openOn(map);
+
+
+map.on('click', onMapClick);
