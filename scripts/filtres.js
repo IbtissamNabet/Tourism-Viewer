9319cdd72bf02eb5ce3ef54e72f675f83ab8a60a
Noellie PAULUS <noelliepaulus@etu.univ-lyon1.fr>
2023-10-05 10:20:30 +0200

    buttons
    Début d'agencement des bouttons de formulaire pour récuperer les lieux choisi
    + séparations js de la carte et js pour la partie filtre

---
+++ scripts/filtres.js
@@ -0,0 +1,21 @@
+/******************************************************************
+ * 
+ * Ce fichier contient toutes les fonctions
+ * nécessaires à la gestion des filtres à appliquer sur la carte
+ * 
+ * ****************************************************************
+ */
+
+/* On range les sortes de Lieux par Types et Types Spécifiques */
+let listeTypesLieux = [];
+let listeLieux = [];
+
+
+/* listeTypesLieux.push(${   nom_t2   }); */
+
+let BtnLieux = document.getElementById("selection-type-lieux");
+
+for(let i = 0; i< ???; i++)
+BtnLieux.innerHTML=`
+    <button id="">${}</button>
+`;
