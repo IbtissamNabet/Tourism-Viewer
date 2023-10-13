6853c418d47a103da41fbf8725c8609e67c15228
Noellie PAULUS <noelliepaulus@etu.univ-lyon1.fr>
2023-10-13 16:06:22 +0200

    fin test boutons
    success, les boutons s'affichent dans la fenêtre html à partir du fichier json

--- testBtnJs/test.js
+++ testBtnJs/test.js
@@ -1,7 +1,27 @@
-const recup = document.querySelector(".Boutons")
+const jslieux = await fetch('types.json');
+const lieux = await jslieux.json();
 
-const test1 = document.createElement("input");
-test1.type = `button`;
-test1.innerText = `Bouton1`;
+let distinctlieux = [];
 
-recup.appendChild(test1);
+/* Pour tous les éléments de lieux, s'il ne corespond
+à aucun des éléments de distinct alors on le rajoute à distinct  */
+for (let i = 0; i < lieux.length; i++){
+
+    let types = lieux[i].ParentLabel;
+
+    if(distinctlieux.includes(types) === false)
+        distinctlieux.push(types);
+}
+
+console.log(distinctlieux)
+
+for (let j = 0 ; j < distinctlieux.length; j++){
+    let bouton = distinctlieux[j];
+
+    const recup = document.querySelector(".Boutons")
+
+    const test1 = document.createElement("button");
+    test1.innerText = bouton;
+
+    recup.appendChild(test1);
+}
