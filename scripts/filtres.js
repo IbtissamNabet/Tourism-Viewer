49cb17d6f9f42b0da8e3a5da96aee990c5575c73
Noellie Paulus <noelliepaulus@etu.univ-lyon1.fr>
2023-10-16 17:55:41 +0200

    classement des sous types (boutons)
    ajout d'une classe en dessou de chaque boutons de lieux
    rangement distinct des sous lieux dans un tableau dédié

--- scripts/filtres.js
+++ scripts/filtres.js
@@ -12,7 +12,7 @@ const rep = await fetch('json/types.json');
 const lieux = await rep.json();
 
 /* Tableau qui contiendra les types généraux des lieux */
-let distinctlieux = [];
+let distinctlieuxgen = [];
 
 /* Pour tous les éléments de lieux, s'il ne corespond
 à aucun des éléments de distinct alors on le rajoute à distinct  */
@@ -20,14 +20,14 @@ for (let i = 0; i < lieux.length; i++){
 
     let types = lieux[i].ParentLabel;
 
-    if(distinctlieux.includes(types) === false)
-        distinctlieux.push(types);
+    if(! distinctlieuxgen.includes(types))
+    distinctlieuxgen.push(types);
 }
 
 
 /* Pour chacun des types généraux de lieux on crée un bouton */
-for (let j = 0 ; j < distinctlieux.length; j++){
-    let bouton = distinctlieux[j];
+for (let j = 0 ; j < distinctlieuxgen.length; j++){
+    let bouton = distinctlieuxgen[j];
 
     const recup = document.querySelector(".selection-type-lieux")
 
@@ -37,4 +37,28 @@ for (let j = 0 ; j < distinctlieux.length; j++){
     buttypes.id = bouton.replace(/\s/g,'');
 
     recup.appendChild(buttypes);
+
+    /* création de classes en dessous de chaque boutons de types
+    de lieux pour insérer plus tard les sous types de lieux*/
+    const butclasses = document.createElement("div");
+    butclasses.classList.add(buttypes.id);
+
+    recup.appendChild(butclasses);
 }
+
+/* Tableau qui contiendra les types spécifiques des lieux */
+let distinctlieuxspe = [];
+
+/* Pour tous les éléments Label de lieux, s'il ne correspond
+à aucun des éléments de distinct alors on le rajoute à distinct  */
+for (let i = 0; i < lieux.length; i++){
+
+    let types = lieux[i].Label;
+
+    if(! distinctlieuxspe.includes(types))
+        distinctlieuxspe.push(types);
+}
+
+// console.log(distinctlieuxspe);
+
+// then affichage des boutons en déroulant
