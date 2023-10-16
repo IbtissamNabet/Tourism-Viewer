67603d9080012360e967c284d8d9e1a9ec22a903
Noellie Paulus <noelliepaulus@etu.univ-lyon1.fr>
2023-10-16 18:52:58 +0200

    boutons sous types de lieux
    affichage partiel des sous types de lieux lors du clic sur un des "sur-types"

--- scripts/filtres.js
+++ scripts/filtres.js
@@ -14,7 +14,7 @@ const lieux = await rep.json();
 /* Tableau qui contiendra les types généraux des lieux */
 let distinctlieuxgen = [];
 
-/* Pour tous les éléments de lieux, s'il ne corespond
+/* Pour tous les éléments de lieux, s'il ne correspond
 à aucun des éléments de distinct alors on le rajoute à distinct  */
 for (let i = 0; i < lieux.length; i++){
 
@@ -23,6 +23,7 @@ for (let i = 0; i < lieux.length; i++){
     if(! distinctlieuxgen.includes(types))
     distinctlieuxgen.push(types);
 }
+
 
 
 /* Pour chacun des types généraux de lieux on crée un bouton */
@@ -34,23 +35,64 @@ for (let j = 0 ; j < distinctlieuxgen.length; j++){
     const buttypes = document.createElement("button");
     buttypes.innerText = bouton;
     /* Création d'identifiants en enlevant les espaces des labels */
-    buttypes.id = bouton.replace(/\s/g,'');
+    buttypes.id = bouton.replace(/\s/g,'-');
 
     recup.appendChild(buttypes);
 
     /* création de classes en dessous de chaque boutons de types
     de lieux pour insérer plus tard les sous types de lieux*/
     const butclasses = document.createElement("div");
-    butclasses.classList.add(buttypes.id);
+    butclasses.classList.add(bouton.replace(/\s/g,'_'));
 
     recup.appendChild(butclasses);
 }
 
-/* Tableau qui contiendra les types spécifiques des lieux */
+
+
+/* Pour chacun des boutons de types de lieux, si cliqué, on affiche
+ses sous-types sous forme également de boutons */
+for (let i = 0 ; i < distinctlieuxgen.length; i++){
+
+    let classeLieux = distinctlieuxgen[i].replace(/\s/g,'_');
+    let idLieux = distinctlieuxgen[i].replace(/\s/g,'-');
+
+    // on sélectionne le bouton qui doit être cliqué
+    let btnLieux = document.getElementById(idLieux);
+    
+    btnLieux.addEventListener("click", () => {
+        console.log("J'ai cliqué sur " + distinctlieuxgen[i]);
+
+        // on récupère l'emplacement dédié au sous types de ces lieux
+        const recupsoustypes = document.querySelector("." + classeLieux);
+
+        for (let j = 0; j < lieux.length; i++){
+            
+            if(lieux[i].ParentLabel.replace(/\s/g,'-') === idLieux){
+
+                const butsoustypes = document.createElement("button");
+                butsoustypes.innerText = lieux[i].Label;
+
+                recupsoustypes.appendChild(butsoustypes);
+            }
+        }
+    })
+}
+
+/**
+ *   element.addEventListener("click", this, false);
+     element.addEventListener("dblclick", this, false);
+ * click normal ou double clique pour différencier l'ouverture ou fermeture des onglets ?
+ */
+
+
+
+/**
+ * Peut servir (fonctionnel)
+
+// Tableau qui contiendra les types spécifiques des lieux
 let distinctlieuxspe = [];
 
-/* Pour tous les éléments Label de lieux, s'il ne correspond
-à aucun des éléments de distinct alors on le rajoute à distinct  */
+// Pour tous les éléments Label de lieux, s'il ne correspond à aucun des éléments de distinct alors on le rajoute à distinct
 for (let i = 0; i < lieux.length; i++){
 
     let types = lieux[i].Label;
@@ -61,4 +103,4 @@ for (let i = 0; i < lieux.length; i++){
 
 // console.log(distinctlieuxspe);
 
-// then affichage des boutons en déroulant
+*/
